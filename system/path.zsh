4544870b2e4166ad5ac74aadc60a95be790db8b2
Paul O’Shannessy <paul@oshannessy.com>
2010-10-14 17:56:13 -0700

    add ccache to my path for speedier compiles

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,6 @@
 export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
+# Add ccache aliases to my path
+export PATH="/usr/local/Cellar/ccache/3.1/libexec:$PATH"
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
