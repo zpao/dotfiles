6225154cbf0dc680d54355f8de10d1e8827a9d8a
Paul O’Shannessy <paul@oshannessy.com>
2010-10-17 22:02:56 -0700

    i have a pythonpath

--- system/path.zsh
+++ system/path.zsh
@@ -7,3 +7,5 @@ export PATH="/opt/local/bin:$PATH"
 export PATH="/usr/local/Cellar/ccache/3.1/libexec:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export PYTHONPATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages:$PYTHONPATH"
