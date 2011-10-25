81d94981bb64a9a9a05fc9030a43f776152799e5
Paul O’Shannessy <paul@oshannessy.com>
2011-10-25 15:24:00 -0700

    make sure NODE_PATH looks in my $HOME/node_modules (this never seemed to work, but I could have just been "doing it wrong")

--- system/path.zsh
+++ system/path.zsh
@@ -9,3 +9,5 @@ export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 export PYTHONPATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages:$PYTHONPATH"
+
+export NODE_PATH=".:$HOME/node_modules:$NODE_PATH"
