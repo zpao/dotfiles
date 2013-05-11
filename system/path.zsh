99b05a5879a0e08475fe1c85c88849a69d4b932f
Paul O’Shannessy <paul@oshannessy.com>
2013-05-11 16:40:23 -0700

    more paths

--- system/path.zsh
+++ system/path.zsh
@@ -3,6 +3,10 @@ export PATH="/opt/local/bin:$PATH"
 
 export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/bin:$PATH"
 
+# npm install -g
+export PATH="/usr/local/share/npm/bin:$PATH"
+
+export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 # Add ccache aliases to my path
 # export PATH="/usr/local/Cellar/ccache/3.1.4/libexec:$PATH"
 
