84d006d94bd1b07c5688fb3ac3b68fa9967d17e0
Paul O’Shannessy <paul@oshannessy.com>
2014-10-27 13:47:01 -0700

    update my own _path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,22 +1,8 @@
-# ~/bin
-export PATH="$HOME/bin:$PATH"
-
-# z (aka build myself into myhome dir)
-export PATH="$HOME/z/bin:$PATH"
-
-# dotfiles bin
-export PATH="$ZSH/bin:$PATH"
-
-# homebrew
-export PATH="/usr/local/bin:$PATH"
-
-# npm
-export PATH="/usr/local/share/npm/bin:$PATH"
-
-# rvm
-export PATH="$HOME/.rvm/bin:$PATH"
-
-# and of course, local ./bin
-export PATH="./bin:$PATH"
+# ./bin - pick up current bin if there is one
+# ~/z/bin - anything in my home directory and custom built (rare)
+# ~/bin - anything in my home directory
+# $ZSH/bin - anything in this repo
+# /usr/local/bin - homebrew (usually), force it higher than it would otherwise be to override system installs
+export PATH="./bin:$HOME/z/bin:$HOME/bin:$ZSH/bin:/usr/local/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
