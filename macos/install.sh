093872cb207678eea2625de73feb24678678a637
Paul O’Shannessy <paul@oshannessy.com>
2017-02-20 13:02:30 -0800

    Convert to Brewfile, specify a couple extra things

--- macos/install.sh
+++ macos/install.sh
@@ -3,5 +3,9 @@
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
 
-echo "› sudo softwareupdate -i -a"
-sudo softwareupdate -i -a
+# zpao: Yea, we aren't going to do this. I don't want to install everything.
+# There's a homebrew extension (mas) that lets us install that way. It *should*
+# work but I'm writing this from a set up machine already.
+
+# echo "› sudo softwareupdate -i -a"
+# sudo softwareupdate -i -a
